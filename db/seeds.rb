08b6595ebacae4c339b1b666e898e634c73e3d44
jchasse <jonathan.r.chasse@gmail.com>
2020-09-13 10:50:28 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+Coupon.create(coupon_code: "134134t1gtbgqrg", store: "Second")
+Coupon.create(coupon_code: "1341vdvav3bgasg", store: "Third")
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
